ebd1993089a9bd499b8948f6fcd0f4cae1955004
DangKien98 <dangvankien98@live.com>
2022-08-16 22:08:44 +0700

    new

--- src/main/java/com/example/vfarmrdbackend/VFarmRdBackEndApplication.java
+++ src/main/java/com/example/vfarmrdbackend/VFarmRdBackEndApplication.java
@@ -3,28 +3,39 @@ package com.example.vfarmrdbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableScheduling
-@CrossOrigin
 public class VFarmRdBackEndApplication {
     public static void main(String[] args) {
         SpringApplication.run(VFarmRdBackEndApplication.class, args);
     }
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*")
-                        .allowedOrigins("*");
-            }
-        };
+    @Configuration
+    public class WebMvcConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedMethods("*")
+                    .allowedOrigins("http://oggycute.tplinkdns.com:31700")
+                    .allowedHeaders("*")
+                    .allowCredentials(false)
+                    .maxAge(-1);
+        }
     }
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                        .allowedHeaders("*")
+//                        .allowedOrigins("*");
+//            }
+//        };
+//    }
 }
