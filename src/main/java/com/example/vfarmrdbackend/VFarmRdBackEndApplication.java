a6912db6ee53fa7338d628f258ce0c2740418b10
DangKien98 <dangvankien98@live.com>
2022-08-16 21:22:36 +0700

    new

--- src/main/java/com/example/vfarmrdbackend/VFarmRdBackEndApplication.java
+++ src/main/java/com/example/vfarmrdbackend/VFarmRdBackEndApplication.java
@@ -2,9 +2,17 @@ package com.example.vfarmrdbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,6 +23,32 @@ public class VFarmRdBackEndApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VFarmRdBackEndApplication.class, args);
+    }
+
+    @Configuration
+    @EnableWebSecurity(debug = true)
+    public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable()
+                    .authorizeRequests()
+                    .anyRequest().permitAll()
+                    .and().httpBasic();
+        }
+
+        @Bean
+        public FilterRegistrationBean corsFilter() {
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            CorsConfiguration config = new CorsConfiguration();
+            config.addAllowedOrigin("*");
+            config.addAllowedHeader("*");
+            config.addAllowedMethod("*");
+            source.registerCorsConfiguration("/**", config);
+            FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+            bean.setOrder(0);
+            return bean;
+        }
     }
 
     @Bean
